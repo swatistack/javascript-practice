ef35a61818d67c86d8e6ece8cc176ffc6037db3c
swatistack <mrtswatichauhan@gmail.com>
2025-07-18 21:16:21 +0530

    Testing Git push

---
+++ nestedObj.js
@@ -0,0 +1,32 @@
+let students = {
+    Swati: {
+        age: 28,
+        city: "Meerut",
+        marks: 89,
+    },
+    Deepaak: {
+        age: 29,
+        city: "Modinagr",
+        marks: "89",
+    }
+}
+
+// Array Object
+let arrObj = [{
+        age: 28,
+        city: "Meerut",
+        marks: 89,
+    },
+    {
+        age: 29,
+        city: "Modinagr",
+        marks: "89",
+    }
+];
+
+let cars = {
+    name: "BMW",
+    model: "XYZ",
+    color: "Red",
+
+}
